b9514f42cf9bb21a2938182caf428536db70e580
devmaslove <63534992+devmaslove@users.noreply.github.com>
2022-09-16 22:44:39 +0300

    Урок 12 конец

--- lib/presentation/easter_egg/easter_egg_page.dart
+++ lib/presentation/easter_egg/easter_egg_page.dart
@@ -24,7 +24,90 @@ class RocketAnimationBody extends StatefulWidget {
   State<RocketAnimationBody> createState() => _RocketAnimationBodyState();
 }
 
-class _RocketAnimationBodyState extends State<RocketAnimationBody> {
+class _RocketAnimationBodyState extends State<RocketAnimationBody>
+    with SingleTickerProviderStateMixin {
+  static const animationDurationSeconds = 10;
+  late AnimationController controller;
+  late Animation<Alignment> rocketPositionAnimation;
+  late Animation<double> rocketScaleAnimation;
+  late Animation<double> fireRotationAnimation;
+  late Animation<double> glowScaleAnimation;
+
+  bool started = false;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: animationDurationSeconds),
+    );
+    controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() => started = false);
+        controller.reset();
+      }
+    });
+    rocketPositionAnimation = AlignmentTween(
+      begin: const Alignment(0, 0.9),
+      end: const Alignment(0, -1),
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: const Interval(0.1, 0.8, curve: Curves.easeInCubic),
+      ),
+    );
+    rocketScaleAnimation = Tween<double>(
+      begin: 1.0,
+      end: 0.0,
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: const Interval(0.3, 0.8, curve: Curves.easeInCubic),
+      ),
+    );
+    fireRotationAnimation = TweenSequence<double>([
+      TweenSequenceItem(
+        tween: Tween<double>(begin: 0.005, end: -0.005),
+        weight: 50,
+      ),
+      TweenSequenceItem(
+        tween: Tween<double>(begin: -0.005, end: 0.005),
+        weight: 50,
+      ),
+    ]).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: const Interval(
+          0.0,
+          0.8,
+          curve: SawTooth(animationDurationSeconds * 5),
+        ),
+      ),
+    );
+    glowScaleAnimation = TweenSequence<double>([
+      TweenSequenceItem(
+        tween: Tween<double>(begin: 0.0, end: 1.0),
+        weight: 50,
+      ),
+      TweenSequenceItem(
+        tween: Tween<double>(begin: 1.0, end: 0.0),
+        weight: 50,
+      ),
+    ]).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: const Interval(0.8, 0.9),
+      ),
+    );
+  }
+
+  void animate() {
+    if (controller.isAnimating) return;
+    setState(() => started = true);
+    controller.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
@@ -51,11 +134,42 @@ class _RocketAnimationBodyState extends State<RocketAnimationBody> {
                   ),
                 ),
               ),
-              Align(
-                alignment: const Alignment(0, 0.9),
-                child: Image.asset(
-                  AppImages.rocketWithoutFire,
-                  height: 200,
+              if (started)
+                AlignTransition(
+                  alignment: rocketPositionAnimation,
+                  child: RotationTransition(
+                    turns: fireRotationAnimation,
+                    child: ScaleTransition(
+                      scale: rocketScaleAnimation,
+                      child: Image.asset(
+                        AppImages.rocketFire,
+                        height: 200,
+                      ),
+                    ),
+                  ),
+                ),
+              Positioned(
+                top: 70,
+                left: constraints.maxWidth / 2 - 40,
+                child: ScaleTransition(
+                  scale: glowScaleAnimation,
+                  child: Image.asset(
+                    AppImages.starGlow,
+                    height: 50,
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () => animate(),
+                child: AlignTransition(
+                  alignment: rocketPositionAnimation,
+                  child: ScaleTransition(
+                    scale: rocketScaleAnimation,
+                    child: Image.asset(
+                      AppImages.rocketWithoutFire,
+                      height: 200,
+                    ),
+                  ),
                 ),
               ),
             ],
